8ef25a56242c980d662f163e73835cd25e0a7580
Prudnik98 <makeme1234576@gmail.com>
2025-02-03 16:31:02 -0600

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1311,7 +1311,7 @@ $(function () {
   var wssport = 8080;
   var isSecure = location.protocol == 'https:';
   var gClient = new Client(`${isSecure ? 'wss' : 'ws'}://${location.host}`);
-  //var gClient = new Client(`${isSecure ? 'wss' : 'ws'}://tin.mpp.8448.space`); // smn serv
+  //var gClient = new Client(`${isSecure ? 'wss' : 'ws'}://multiplayerpiano.io`); // smn serv
   if (loginInfo) {
     gClient.setLoginInfo(loginInfo);
   }
